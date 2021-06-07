e0d5dddac3a9d97155b084c0c3f4e79e14a5d9a7
山下彩友子 <yamashita-ayuko@esz.co.jp>
2021-06-07 22:09:40 +0900

    font family

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import QuizList from './component/quiz_list';
 import './css/reset.css';
+import './css/base.scss';
 
 ReactDOM.render(<QuizList/>, document.getElementById("root"));
