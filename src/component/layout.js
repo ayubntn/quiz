8750534a3537d01e3e9ba42d43d170ae919447ad
山下彩友子 <yamashita-ayuko@esz.co.jp>
2021-06-21 19:09:06 +0900

    no message

--- src/component/layout.js
+++ src/component/layout.js
@@ -12,9 +12,10 @@ class Layout extends React.Component {
 
 		let beforeLink;
 		if (currentNumber === 1) {
-			beforeLink = <Link to="/quiz">さいしょ</Link>
+			beforeLink = <Link to="/quiz" className={styles.headerHome}><i className="fas fa-home" />さいしょ</Link>
 		} else {
-			beforeLink = <Link to={'/' + this.props.type + '/' + (currentNumber - 1)}>まえ</Link>
+			beforeLink = <Link to={'/' + this.props.type + '/' + (currentNumber - 1)} className={styles.headerBefore}>
+				<i className="fas fa-arrow-left" />まえ</Link>
 		}
 
 		let nextLink;
@@ -23,11 +24,9 @@ class Layout extends React.Component {
 			nextLink = <Link to={'/' + this.props.type + '/result'}>けっか</Link>;
 			nextBtn = <Link className={styles.btn} to={'/' + this.props.type + '/result'}>けっか</Link>;
 		} else {
-			nextLink = <Link to={'/' + this.props.type + '/' + (currentNumber + 1)}>つぎ</Link>;
+			nextLink = <Link to={'/' + this.props.type + '/' + (currentNumber + 1)} className={styles.headerNext}>つぎ<i className="fas fa-arrow-right" /></Link>;
 			nextBtn = <Link className={styles.btn} to={'/' + this.props.type + '/' + (currentNumber + 1)}>つぎ</Link>;
 		}
-
-		const answeredCount = this.props.answers.filter(value => value != null).length;
 
 		return (
 			<div className={styles.layout}>
@@ -48,8 +47,8 @@ class Layout extends React.Component {
 				<nav className={styles.footer}>
 					<div className={styles.progress}>
 						<progress max="100"
-								  value={answeredCount / this.props.data.length * 100}>
-							{answeredCount / this.props.data.length * 100}
+								  value={currentNumber / this.props.data.length * 100}>
+							{currentNumber / this.props.data.length * 100}
 						</progress>
 					</div>
 				</nav>
--- src/component/result.js
+++ src/component/result.js
@@ -3,7 +3,29 @@ import {Link} from 'react-router-dom';
 import title from '../data/title';
 import styles from '../css/module/result.module.scss';
 
+import donmai from '../image/result_donmai.png';
+import yourBest from '../image/result_your_best.png';
+import great from '../image/result_great.png';
+import congrats from '../image/result_congrats.png';
+
+const rankImages = {
+	expert: congrats,
+	pro: great,
+	beginner: yourBest,
+	baby: donmai
+};
+
 class Result extends React.Component {
+
+	constructor(props) {
+		super(props);
+		const score = this.scoring();
+		const rank = this.ranking(score);
+		this.state = {
+			score: score,
+			rank: rank,
+		};
+	}
 
 	scoring() {
 		return this.props.answers.filter((answer, i) => {
@@ -11,11 +33,24 @@ class Result extends React.Component {
 		}).length;
 	}
 
+	ranking(score) {
+		const per = score / this.props.data.length * 100;
+		let rank = 'baby';
+		if (per === 100) {
+			rank = 'expert';
+		} else if (per >= 66) {
+			rank = 'pro';
+		} else if (per >= 33) {
+			rank = 'beginner';
+		}
+		return rank;
+	}
+
 	render() {
-		const score = this.scoring();
+		const score = this.state.score;
 		const name = title[this.props.type];
 		let special;
-		if (score === this.props.data.length) {
+		if (this.state.rank === 'expert') {
 			special = <p className={styles.special}>きみは {name}はかせ だ！</p>;
 		} else {
 			special = <p className={styles.special}>また あそんでね</p>;
@@ -28,7 +63,9 @@ class Result extends React.Component {
 					<strong className={styles.number}>{this.props.data.length}</strong>もんちゅう<br/>
 					<strong className={styles.number}>{score}</strong>もん せいかい</p>
 				{special}
-
+				<div className={styles.resultImage}>
+					<img src={rankImages[this.state.rank]}/>
+				</div>
 				<Link className={styles.back} to="/quiz">もどる</Link>
 
 			</div>
