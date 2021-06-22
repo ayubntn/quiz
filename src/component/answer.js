fb9d620b01d4c43bba2e4563d0d1934ab96ce8dc
山下彩友子 <yamashita-ayuko@esz.co.jp>
2021-06-22 21:00:40 +0900

    副作用フック

--- src/component/answer.js
+++ src/component/answer.js
@@ -1,27 +1,25 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styles from '../css/module/answer.module.scss';
 
-class Answer extends React.Component {
+function Answer(props) {
 
-	componentDidMount() {
+	useEffect(() => {
 		let element = document.documentElement;
 		let bottom = element.scrollHeight - element.clientHeight;
 		window.scroll({top: bottom, behavior: 'smooth'});
-	}
+	});
 
-	render() {
-		let title = 'ざんねん！';
-		if (this.props.solved) {
-			title = 'せいかい！'
-		}
-		return (
-			<div className={styles.answer}>
-				<p className={styles.title}><span className={this.props.solved ? styles.maru : styles.batsu}/>{title}</p>
-				<p className={styles.rightAnswer}>こたえ {this.props.data.options[this.props.data.rightAnswerIdx]}</p>
-				<p className={styles.description}>{this.props.data.description}</p>
-			</div>
-		)
+	let title = 'ざんねん！';
+	if (props.solved) {
+		title = 'せいかい！'
 	}
+	return (
+		<div className={styles.answer}>
+			<p className={styles.title}><span className={props.solved ? styles.maru : styles.batsu}/>{title}</p>
+			<p className={styles.rightAnswer}>こたえ {props.data.options[props.data.rightAnswerIdx]}</p>
+			<p className={styles.description}>{props.data.description}</p>
+		</div>
+	)
 }
 
 export default Answer;
